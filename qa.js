08622abbedcb025821f3b5cc96108686cc42b752
Joshua <13402946+nomadicTree@users.noreply.github.com>
2024-07-19 14:48:37 +0100

    reorganise questions

--- qa.js
+++ qa.js
@@ -1,56 +1,70 @@
-const questionData = {
-    questions: [
+const questionData = { 
+    systemsArchitecture: [
+        {
+            question: "What are the main components of the CPU?",
+            answer: "Cache, clock, ALU, registers, control unit"
+        },
+        {
+            question: "What does ALU stand for?",
+            answer: "Arithmetic logic unit"
+        },
+        {
+            question: "What are the the names of the registers in the CPU?",
+            answer: "Memory address register, Memory data register, accumulator, program counter"
+        },
+        {
+            question: "What does the control unit do?",
+            answer: "Fetches, decodes, and executes instructions; moves data around the CPU; transmits control signals to other components of the CPU"
+        },
+        {
+            question: "What does the Arithmetic Logic Unit do?",
+            answer: "Performs arithmetic and logic calculations"
+        },
+        {
+            question: "What does cache do in the CPU?",
+            answer: "Stores recently and frequently used instructions and data"
+        },
+        {
+            question: "What does MDR stand for?",
+            answer: "Memory Data Register"
+        },
+        {
+            question: "What does the MDR store?",
+            answer: "The contents of the address held in the MAR, or data which is about to be written to RAM"
+        },
+        {
+            question: "What does MAR stand for?",
+            answer: "Memory address register"
+        },
+        {
+            question: "What does the MAR store?",
+            answer: "The memory address from which data will be fetched, or the address to which data will be sent"
+        },
+        {
+            question: "What does the Accumulator store?",
+            answer: "Results of calculations performed by the Arithmetic Logic Unit"
+        },
+        {
+            question: "What does the Arithmetic Logic Unit do?",
+            answer: "Performs arithmetic and logic calculations"
+        },
+        {
+            question: "What does the Arithmetic Logic Unit do?",
+            answer: "Performs arithmetic and logic calculations"
+        },
         {
             question: "What does CPU stand for?",
             answer: "Central Processing Unit"
-        },
+        }
+    ],
+    memoryAndStorage: [
         {
-            question: "What is the purpose of a compiler?",
-            answer: "To translate high-level programming languages into machine code."
+            question: "What is stored in RAM?",
+            answer: "Instructions and data currently in use"
         },
         {
             question: "Explain the difference between RAM and ROM.",
             answer: "RAM (Random Access Memory) is volatile memory used for temporary storage, while ROM (Read-Only Memory) is non-volatile memory used for permanent storage."
-        },
-        {
-            question: "What does HTML stand for?",
-            answer: "HyperText Markup Language"
-        },
-        {
-            question: "Name two types of loops used in programming.",
-            answer: "Count-controlled (e.g. for) loop and condition-controlled (e.g. while, do ... until) loop"
-        },
-        {
-            question: "What is the primary function of an operating system?",
-            answer: "To manage computer hardware and software resources and provide common services for computer programs."
-        },
-        {
-            question: "Differentiate between a LAN and a WAN.",
-            answer: "LAN (Local Area Network) is a network confined to a small geographic area, whereas WAN (Wide Area Network) covers a large geographic area."
-        },
-        {
-            question: "Define the term \"encryption.\"",
-            answer: "The process of encoding information so that only authorized parties can access it."
-        },
-        {
-            question: "What is a firewall used for in computing?",
-            answer: "To monitor and control incoming and outgoing network traffic based on predetermined security rules."
-        },
-        {
-            question: "Name technologies commonly used for web development.",
-            answer: "HTML, CSS, JavaScript"
-        },
-        {
-            question: "What is the difference between a procedure and a function?",
-            answer: "A function returns a value whereas a procedure does not."
-        },
-        {
-            question: "Explain the difference between HTTP and HTTPS.",
-            answer: "HTTP (HyperText Transfer Protocol) is unsecured, while HTTPS (HyperText Transfer Protocol Secure) encrypts data to ensure secure communication over a computer network."
-        },
-        {
-            question: "Define the term \"algorithmic efficiency.\"",
-            answer: "The measure of how well an algorithm performs in terms of time and space requirements as the size of the input to the algorithm increases."
         },
         {
             question: "Is RAM volatile or non-volatile?",
@@ -71,14 +85,25 @@ const questionData = {
         {
             question: "What does SSD stand for?",
             answer: "Solid State Drive"
+        }
+
+    ],
+    computerNetworksConnectionsAndProtocols: [
+        {
+            question: "What is a protocol?",
+            answer: "A set of rules for data communication"
         },
         {
-            question: "Name an example of an operating system.",
-            answer: "Windows, macOS, Linux, iOS, Android"
+            question: "What is a data packet?",
+            answer: "A unit of data transmitted over a network"
         },
         {
-            question: "What does GUI stand for?",
-            answer: "Graphical User Interface"
+            question: "Differentiate between a LAN and a WAN.",
+            answer: "LAN (Local Area Network) is a network confined to a small geographic area, whereas WAN (Wide Area Network) covers a large geographic area."
+        },
+        {
+            question: "Explain the difference between HTTP and HTTPS.",
+            answer: "HTTP (HyperText Transfer Protocol) is unsecured, while HTTPS (HyperText Transfer Protocol Secure) encrypts data to ensure secure communication over a computer network."
         },
         {
             question: "What is a network?",
@@ -107,15 +132,114 @@ const questionData = {
         {
             question: "What is the function of a router?",
             answer: "To direct data packets between networks"
+        }
+
+    ],
+    networkSecurity: [
+        {
+            question: "What is a firewall used for in computing?",
+            answer: "To monitor and control incoming and outgoing network traffic based on predetermined security rules."
+        }
+    ],
+    ethics: [
+
+    ],
+    algorithms: [
+
+    ],
+    programmingFundamentals: [
+        {
+            question: "Name two types of loops used in programming.",
+            answer: "Count-controlled (e.g. for) loop and condition-controlled (e.g. while, do ... until) loop"
         },
         {
-            question: "What is an algorithm?",
-            answer: "A step-by-step procedure for solving a problem"
+            question: "What is the difference between a procedure and a function?",
+            answer: "A function returns a value whereas a procedure does not."
         },
         {
             question: "What is a variable in programming?",
             answer: "A named location in memory whose value can change while the program is running."
         },
+        {
+            question: "What are the three fundamental programming constructs?",
+            answer: "Sequence, selection, and iteration"
+        },
+        {
+            question: "What is an array?",
+            answer: "A collection of elements identified by index or key"
+        },
+        {
+            question: "What is a boolean data type?",
+            answer: "A data type with two possible values: true or false"
+        },
+
+        {
+            question: "What is pseudocode?",
+            answer: "A simplified, informal language used to outline an algorithm"
+        }
+    ],
+    producingRobustPrograms: [
+
+    ],
+    booleanLogic: [
+
+    ],
+    programmingLanguagesIDEs: [
+        {
+            question: "What is the purpose of a compiler?",
+            answer: "To translate high-level programming languages into machine code."
+        },
+        {
+            question: "What is a compiler?",
+            answer: "A program that translates source code into executable code"
+        },
+        {
+            question: "What does IDE stand for?",
+            answer: "Integrated Development Environment"
+        }
+    ],
+    generalKnowledge: [
+        {
+            question: "What does HTML stand for?",
+            answer: "HyperText Markup Language"
+        }
+    ],
+    sortMe: [
+        {
+            question: "What is the primary function of an operating system?",
+            answer: "To manage computer hardware and software resources and provide common services for computer programs."
+        },
+
+        {
+            question: "Define the term \"encryption.\"",
+            answer: "The process of encoding information so that only authorized parties can access it."
+        },
+
+        {
+            question: "Name technologies commonly used for web development.",
+            answer: "HTML, CSS, JavaScript"
+        },
+
+
+        {
+            question: "Define the term \"algorithmic efficiency.\"",
+            answer: "The measure of how well an algorithm performs in terms of time and space requirements as the size of the input to the algorithm increases."
+        },
+
+        {
+            question: "Name an example of an operating system.",
+            answer: "Windows, macOS, Linux, iOS, Android"
+        },
+        {
+            question: "What does GUI stand for?",
+            answer: "Graphical User Interface"
+        },
+
+        {
+            question: "What is an algorithm?",
+            answer: "A step-by-step procedure for solving a problem"
+        },
+
         {
             question: "What does CSS stand for?",
             answer: "Cascading Style Sheets"
@@ -144,30 +268,7 @@ const questionData = {
             question: "What does URL stand for?",
             answer: "Uniform Resource Locator"
         },
-        {
-            question: "What are the three fundamental programming constructs?",
-            answer: "Sequence, selection, and iteration"
-        },
-        {
-            question: "What does IDE stand for?",
-            answer: "Integrated Development Environment"
-        },
-        {
-            question: "What is an array?",
-            answer: "A collection of elements identified by index or key"
-        },
-        {
-            question: "What is a boolean data type?",
-            answer: "A data type with two possible values: true or false"
-        },
-        {
-            question: "What is a compiler?",
-            answer: "A program that translates source code into executable code"
-        },
-        {
-            question: "What is pseudocode?",
-            answer: "A simplified, informal language used to outline an algorithm"
-        },
+
         {
             question: "What does ASCII stand for?",
             answer: "American Standard Code for Information Interchange"
@@ -259,71 +360,7 @@ const questionData = {
         {
             question: "What is the purpose of a flowchart?",
             answer: "To visually represent the steps of an algorithm or process"
-        },
-        {
-            question: "What is a protocol?",
-            answer: "A set of rules for data communication"
-        },
-        {
-            question: "What is a data packet?",
-            answer: "A unit of data transmitted over a network"
-        },
-        {
-            question: "What is stored in RAM?",
-            answer: "Instructions and data currently in use"
-        },
-        {
-            question: "What are the main components of the CPU?",
-            answer: "Cache, clock, ALU, registers, control unit"
-        },
-        {
-            question: "What does ALU stand for?",
-            answer: "Arithmetic logic unit"
-        },
-        {
-            question: "What are the the names of the registers in the CPU?",
-            answer: "Memory address register, Memory data register, accumulator, program counter"
-        },
-        {
-            question: "What does the control unit do?",
-            answer: "Fetches, decodes, and executes instructions; moves data around the CPU; transmits control signals to other components of the CPU"
-        },
-        {
-            question: "What does the Arithmetic Logic Unit do?",
-            answer: "Performs arithmetic and logic calculations"
-        },
-        {
-            question: "What does cache do in the CPU?",
-            answer: "Stores recently and frequently used instructions and data"
-        },
-        {
-            question: "What does MDR stand for?",
-            answer: "Memory Data Register"
-        },
-        {
-            question: "What does the MDR store?",
-            answer: "The contents of the address held in the MAR, or data which is about to be written to RAM"
-        },
-        {
-            question: "What does MAR stand for?",
-            answer: "Memory address register"
-        },
-        {
-            question: "What does the MAR store?",
-            answer: "The memory address from which data will be fetched, or the address to which data will be sent"
-        },
-        {
-            question: "What does the Accumulator store?",
-            answer: "Results of calculations performed by the Arithmetic Logic Unit"
-        },
-        {
-            question: "What does the Arithmetic Logic Unit do?",
-            answer: "Performs arithmetic and logic calculations"
-        },
-        {
-            question: "What does the Arithmetic Logic Unit do?",
-            answer: "Performs arithmetic and logic calculations"
-        },
+        }
 
     ]
 };
